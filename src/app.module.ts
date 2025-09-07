5652111a07fef7128c81b91d8a5885887f1bb52f
MilitsaTsvetkova <militsa88@yahoo.com>
2025-09-07 16:48:13 +0300

    durable providers: multi-tenancy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { DataSourceModule } from './data-source/data-source.module';
+import { UsersModule } from './users/users.module';
+import { ContextIdFactory } from '@nestjs/core';
+import { AggregateByTenantContextIdStrategy } from './core/aggregate-by-tenant.strategy';
 
+ContextIdFactory.apply(new AggregateByTenantContextIdStrategy());
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, DataSourceModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
