5e3981237093183c8e06903265d8f2cf884b633b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-09-07 17:18:04 +0300

    Durable Providers i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { DataSourceModule } from './data-source/data-source.module';
 import { UsersModule } from './users/users.module';
 import { ContextIdFactory } from '@nestjs/core';
 import { AggregateByTenantContextIdStrategy } from './core/aggregate-by-tenant.strategy';
+import { I18nModule } from './i18n/i18n.module';
+import { AggregateByLocaleContextIdStrategy } from './core/aggregate-by-locale.strategy';
 
 ContextIdFactory.apply(new AggregateByTenantContextIdStrategy());
+ContextIdFactory.apply(new AggregateByLocaleContextIdStrategy());
 @Module({
-  imports: [CoffeesModule, DataSourceModule, UsersModule],
+  imports: [CoffeesModule, DataSourceModule, UsersModule, I18nModule],
   controllers: [AppController],
   providers: [AppService],
 })
