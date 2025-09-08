f911ae40eab7e39d3fd4b31bd97766c258e12a1c
MilitsaTsvetkova <militsa88@yahoo.com>
2025-09-08 12:23:44 +0300

    initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { AlarmsModule } from './alarms/alarms.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, AlarmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
