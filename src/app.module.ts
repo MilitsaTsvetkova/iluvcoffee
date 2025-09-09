efd62ea8c377c432e8a6fcdb668190c92ff2175b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-09-09 09:49:09 +0300

    Experimenting with CQRS. Part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { ApplicationBootstrapOptions } from './common/interfaces/application-bootstrap-options.interface';
 import { AlarmsInfrastructureModule } from './alarms/infrastructure/alarms-infrastucture.module';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CqrsModule.forRoot(), CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
