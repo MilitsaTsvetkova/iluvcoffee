dbdc57bbe5e9f6269539e99e6c0bcc77f5684b19
MilitsaTsvetkova <militsa88@yahoo.com>
2025-09-17 10:39:41 +0300

    Adding Public Routes

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AuthenticationController } from './authentication/authentication.controller';
-import { AuthenticationService } from './authentication/authentication.service';
-import { BcryptService } from './hashing/bcrypt.service';
-import { HashingService } from './hashing/hashing.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
-import { JwtModule } from '@nestjs/jwt';
-import jwtConfig from '../../jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import jwtConfig from '../../jwt.config';
+import { User } from '../users/entities/user.entity';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
+import { BcryptService } from './hashing/bcrypt.service';
+import { HashingService } from './hashing/hashing.service';
 import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 
 @Module({
@@ -22,8 +23,9 @@ import { AccessTokenGuard } from './authentication/guards/access-token/access-to
     AuthenticationService,
     {
       provide: APP_GUARD,
-      useClass: AccessTokenGuard,
+      useClass: AuthenticationGuard,
     },
+    AccessTokenGuard,
   ],
   controllers: [AuthenticationController],
 })
